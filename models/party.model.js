fff60c78d0e6d0e6a0574485c619eff92ad11a2d
Bhavesh Daipuria <bdaipuria@gmail.com>
2025-10-25 00:29:59 +0530

    fix: fields fix for creating parties

--- models/party.model.js
+++ models/party.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const partySchema = new mongoose.Schema({
   party: { type: String, required: true, unique: true },
-  color_code: { type: String, default: "#000000", required: true },
+  color_code: { type: String, default: "#000000" },
   party_logo: { type: String },
 });
 
